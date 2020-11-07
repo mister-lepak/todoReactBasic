044ddcdb80477912379efbce0a841d3fb043fc27
Try Wijono <3wijono@gmail.com>
2020-11-07 21:17:22 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Overview from "./components/Overview";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: "",
+      tasks: [],
+      id: [],
+      edit: [],
+      editTask: "",
+    };
+
+    this.onDelete = this.onDelete.bind(this);
+    this.onEdit = this.onEdit.bind(this);
+    this.onSubmitEdit = this.onSubmitEdit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.editChange = this.editChange.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: e.target.value,
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: "",
+      id: this.state.id.concat(this.state.id.length + 1),
+      edit: this.state.edit.concat(false),
+    });
+  };
+
+  onDelete = (e) => {
+    this.setState({
+      tasks: this.state.tasks.filter((el) => el !== e),
+    });
+  };
+
+  onEdit = (id) => {
+    this.setState({
+      edit: this.state.edit.map((el, index) => {
+        return index === id ? true : false;
+      }),
+      editTask: "",
+    });
+  };
+
+  editChange = (e) => {
+    this.setState({ editTask: e.target.value });
+  };
+
+  onSubmitEdit = (index) => {
+    this.setState({
+      tasks: this.state.tasks.map((task, id) => {
+        return id === index ? this.state.editTask : task;
+      }),
+    });
+    this.onEdit();
+  };
+
+  render() {
+    const { task, tasks, edit, id, editTask } = this.state;
+
+    return (
+      <div>
+        <div className="ui two column centered grid">
+          <div className="column">
+            <form className="ui form" onSubmit={this.onSubmitTask}>
+              <div className="field">
+                <label>Input your task</label>
+                <input
+                  onChange={this.handleChange}
+                  value={task}
+                  type="text"
+                  name="task"
+                  placeholder="Put your task"
+                ></input>
+              </div>
+              <button className="ui button" type="submit">
+                Add Task
+              </button>
+            </form>
+          </div>
+        </div>
+        <Overview
+          tasks={tasks}
+          id={id}
+          onDelete={this.onDelete}
+          edit={edit}
+          editTask={editTask}
+          onEdit={this.onEdit}
+          onSubmitEdit={this.onSubmitEdit}
+          editChange={this.editChange}
+        ></Overview>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,79 @@
+import React from "react";
+import uniqid from "uniqid";
+
+const Overview = (props) => {
+  const {
+    tasks,
+    onDelete,
+    edit,
+    editTask,
+    onEdit,
+    onSubmitEdit,
+    editChange,
+  } = props;
+
+  return (
+    <div className="ui one column centered grid">
+      {tasks.map((e, index) => {
+        return (
+          <>
+            {edit[index] ? (
+              <>
+                <div className="ten wide column ui grid">
+                  <div className="one wide column">{tasks.indexOf(e)}.</div>
+                  <div className="thirteen wide column" key={uniqid()}>
+                    <input
+                      onChange={editChange}
+                      value={editTask}
+                      type="text"
+                      autoFocus="true"
+                    ></input>
+                  </div>
+                  <div className="one wide column">
+                    <button
+                      className="save icon"
+                      // type="submit"
+                      onClick={() => onSubmitEdit(index)}
+                    >
+                      save
+                    </button>
+                  </div>
+                  <div className="one wide column">
+                    <i
+                      className="undo icon"
+                      onClick={() => {
+                        onEdit();
+                      }}
+                    ></i>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="ten wide column ui grid">
+                  <div className="one wide column">{tasks.indexOf(e)}.</div>
+                  <div className="thirteen wide column" key={uniqid()}>
+                    {e}
+                  </div>
+                  <div className="one wide column">
+                    <i className="edit icon" onClick={() => onEdit(index)}></i>
+                  </div>
+                  <div className="one wide column">
+                    <i
+                      className="close icon"
+                      onClick={() => {
+                        onDelete(e);
+                      }}
+                    ></i>
+                  </div>
+                </div>
+              </>
+            )}
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Overview;
